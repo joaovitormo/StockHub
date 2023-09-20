d3fbd21fa332c08fa226138867463a301b921114
João <vitormendes388@gmail.com>
2023-09-20 09:40:51 -0300

    add firebase register

--- app/src/main/java/com/joaovitormo/stockhub/activity/register/RegisterActivity.kt
+++ app/src/main/java/com/joaovitormo/stockhub/activity/register/RegisterActivity.kt
@@ -1,12 +1,70 @@
 package com.joaovitormo.stockhub.activity.register
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.FirebaseNetworkException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException
+import com.google.firebase.auth.FirebaseAuthUserCollisionException
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException
 import com.joaovitormo.stockhub.R
+import com.joaovitormo.stockhub.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+    private val auth = FirebaseAuth.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Remove ActionBar
+        supportActionBar?.hide()
+
+        binding.btCadastrar.setOnClickListener {
+            //Close keyboard after click
+            binding.editEmail.onEditorAction(EditorInfo.IME_ACTION_DONE)
+            binding.editSenha.onEditorAction(EditorInfo.IME_ACTION_DONE)
+
+            val email = binding.editEmail.text.toString()
+            val senha = binding.editSenha.text.toString()
+
+            if(email.isEmpty() || senha.isEmpty()){
+                val snackbar = Snackbar.make(it, "Preencha todos os campos!", Snackbar.LENGTH_SHORT)
+                snackbar.setBackgroundTint(Color.RED)
+                snackbar.show()
+            } else{
+                auth.createUserWithEmailAndPassword(email,senha).addOnCompleteListener { cadastro ->
+                    if (cadastro.isSuccessful) {
+                        val snackbar = Snackbar.make(it, "Sucesso ao cadastrar usuário!", Snackbar.LENGTH_SHORT)
+                        //snackbar.setBackgroundTint(Color.GREEN)
+                        snackbar.show()
+                        binding.editEmail.setText("")
+                        binding.editSenha.setText("")
+                    }
+                }.addOnFailureListener { exception ->
+                    val mensagemErro = when(exception){
+                        is FirebaseAuthWeakPasswordException -> "Digite uma senha com no mínimo 6 caracteres!"
+                        is FirebaseAuthInvalidCredentialsException -> "Digite um e-mail válido!"
+                        is FirebaseAuthUserCollisionException -> "Esta conta já foi cadastrada!"
+                        is FirebaseNetworkException -> "Sem conexão com a internet!"
+                        else -> "Erro ao cadastrar usuário!"
+                    }
+                    val snackbar = Snackbar.make(it, mensagemErro, Snackbar.LENGTH_SHORT)
+                    snackbar.setBackgroundTint(Color.RED)
+                    snackbar.show()
+
+                }
+            }
+
+        }
+
+
     }
 }
