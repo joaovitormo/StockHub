df0aee734c05a088b9a0f3982dba89267b738610
João Vitor <vitormendes388@gmail.com>
2023-11-26 21:44:08 -0300

    add check Internet

--- app/src/main/java/com/joaovitormo/stockhub/activity/homePage/HomePageActivity.kt
+++ app/src/main/java/com/joaovitormo/stockhub/activity/homePage/HomePageActivity.kt
@@ -1,13 +1,21 @@
 package com.joaovitormo.stockhub.activity.homePage
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.firebase.auth.FirebaseAuth
 import com.joaovitormo.stockhub.R
 import com.joaovitormo.stockhub.activity.login.LoginActivity
 import com.joaovitormo.stockhub.databinding.ActivityHomePageBinding
@@ -34,5 +42,70 @@ class HomePageActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        // handle the button click to trigger
+        // checkForInternet function
+        // and show the Toast message according to it.
+        if (checkForInternet(this)) {
+            //Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show()
+        } else {
+            val builder = AlertDialog.Builder(this)
+            builder.setMessage("Você está sem conexão de internet. Por favor entre novamente!")
+                .setCancelable(false)
+                .setPositiveButton("Ok") { dialog, id ->
+                    logout()
+                }
+
+            val alert = builder.create()
+            alert.show()
+        }
+    }
+
+    fun logout() {
+        val auth = FirebaseAuth.getInstance()
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        this?.finish()
+        auth.signOut()
+    }
+
+    @SuppressLint("ServiceCast")
+    private fun checkForInternet(context: Context): Boolean {
+
+        // register activity with the connectivity manager service
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+        // if the android version is equal to M
+        // or greater we need to use the
+        // NetworkCapabilities to check what type of
+        // network has the internet connection
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            // Returns a Network object corresponding to
+            // the currently active default data network.
+            val network = connectivityManager.activeNetwork ?: return false
+
+            // Representation of the capabilities of an active network.
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when {
+                // Indicates this network uses a Wi-Fi transport,
+                // or WiFi has network connectivity
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+
+                // Indicates this network uses a Cellular transport. or
+                // Cellular has network connectivity
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+                // else return false
+                else -> false
+            }
+        } else {
+            // if the android version is below M
+            @Suppress("DEPRECATION") val networkInfo =
+                connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
     }
 }
---
+++ app/src/main/java/com/joaovitormo/stockhub/activity/reportProducts/ReportProductsActivity.kt
@@ -0,0 +1,12 @@
+package com.joaovitormo.stockhub.activity.reportProducts
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.joaovitormo.stockhub.R
+
+class ReportProductsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report_products)
+    }
+}
